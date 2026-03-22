84d5fe567adf2100cfe465cf997b483a0c2ad17f
DS_Lukas <metzi2701@gmail.com>
2026-03-22 18:13:42 +0100

    correction

--- src/exception.py
+++ src/exception.py
@@ -1,15 +1,15 @@
 import sys
-import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     # on wich file the exception occur
     _,_,exc_tb=error_detail.exc_info()
     #save the filename
     file_name = exc_tb.tb_frame.f_code.co_filename
-    erro_message ="Error occured in script name [{0}] line number [{1}] error message [{2}]".format(
+    error_message ="Error occured in script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
-    return erro_message 
+    return error_message 
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
@@ -20,4 +20,10 @@ class CustomException(Exception):
         return self.error_message
     
 
-    
+if __name__=="__main__":
+    try:
+        a=1/0
+
+    except Exception as e:
+        logging.info("div")
+        raise CustomException(e,sys)
