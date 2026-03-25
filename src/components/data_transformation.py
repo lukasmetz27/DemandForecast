2f824c38b79c798279bf8565bbc86203d4b6ae07
DS_Lukas <metzi2701@gmail.com>
2026-03-25 20:52:30 +0100

    data_transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -56,4 +56,15 @@ class DataTransformation:
             raise CustomException(e, sys)
         
 
-        
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read train and test completed")
+
+            preprocessing_obj=self.get_data_transformer_object()
+            target_column_name = 
+
+        except:
+            pass
